d79a929bead423496b065812514ed817464c0c49
Anurag Adhikary <nianurag@gmail.com>
2015-09-01 17:37:46 +0530

    Angular 101: show data from api call in dom

--- client/partial/home/home.js
+++ client/partial/home/home.js
@@ -2,15 +2,11 @@ angular.module('frontend').controller('HomeCtrl',function($scope,$http){
 
 console.log("Home controller has been successfully loaded");
 
-// Simple GET request example :
 $http.get('http://localhost:9090/').
   then(function(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-    console.log("Hey it was successfully requested" + JSON.stringify(response));
+    console.log("Hey it was successfully requested" + JSON.stringify(response.data.reply));
+    $scope.response = response.data.reply;
   }, function(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
     console.log("Some error happened");
   });
 
