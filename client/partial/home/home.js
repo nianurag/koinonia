58fe824a410b962d1e1c59a46470653de046570b
Anurag Adhikary <nianurag@gmail.com>
2015-09-01 17:17:01 +0530

    Make an api call to clojure server

--- client/partial/home/home.js
+++ client/partial/home/home.js
@@ -1,4 +1,17 @@
-angular.module('frontend').controller('HomeCtrl',function($scope){
+angular.module('frontend').controller('HomeCtrl',function($scope,$http){
 
+console.log("Home controller has been successfully loaded");
+
+// Simple GET request example :
+$http.get('http://localhost:9090/').
+  then(function(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+    console.log("Hey it was successfully requested" + JSON.stringify(response));
+  }, function(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    console.log("Some error happened");
+  });
 
 });
