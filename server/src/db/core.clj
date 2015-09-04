732ea53d81b71ddd9da249a34410de474e1e17d1
Anurag Adhikary <nianurag@gmail.com>
2015-09-04 18:41:39 +0530

    Change sha1 to sha224 f when storing to db

--- server/src/db/core.clj
+++ server/src/db/core.clj
@@ -1,7 +1,7 @@
 (ns db.core)
 
 (require '[clojure.java.jdbc :as db])
-(require '[pandect.algo.sha1 :refer :all]
+(require '[pandect.algo.sha224 :refer :all]
          '[clojure.java.io :as io])
 
 (def mysql-db {:subprotocol "mysql"
@@ -13,12 +13,12 @@
   (println "register-db was called")
   (db/insert! mysql-db :User
               {:nick nick
-               :password (sha1 password)
-               :sessionId (sha1 (str nick password))}))
+               :password (sha224 password)
+               :sessionId (sha224 (str nick password))}))
 
 (defn login-db [nick password]
   (println "login-db was called")
-  (get (first (db/query mysql-db ["select * from User where nick = ? and password = ?" nick (sha1 password)])) :sessionid))
+  (get (first (db/query mysql-db ["select * from User where nick = ? and password = ?" nick (sha224 password)])) :sessionid))
 
 (defn get-sessionId [nick]
   (println "get-sessionId was called")
